7fa90fd518b923faf1e6cd416be59cb410db60d1
vitor-oli <vitorbarbosa@discente.ufg.br>
2024-04-27 16:52:22 -0300

    Criação do projeto

---
+++ src/main/java/com/example/clientescadastro/ClientesCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientescadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clientescadastro/ClientesCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientescadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
